2419105370dc9c61ffbefbc9805277ef96923a5f
Gurupoorna <gurupoorna16@gmail.com>
2024-11-03 13:57:07 +0530

    included pos tags into svc.

--- NER/NER.py
+++ NER/NER.py
@@ -9,9 +9,9 @@ import numpy as np
 from itertools import chain
 import pickle
 
-with open('svmclassifier-10000.pkl','rb') as f:
+with open('svmclassifier-5000.pkl','rb') as f:
     svmclassifier = pickle.load(f)
-with open(f'red2v-10000.pkl', 'rb') as f:
+with open(f'red2v-5000.pkl', 'rb') as f:
     red2v = pickle.load(f)
 
 def load_gazetteer_dict():
@@ -29,9 +29,9 @@ g_dict = load_gazetteer_dict()
 g_dict.keys()
 
 def word2features(i, wordseq):
-    wi = wordseq[i]
+    wi, postag = wordseq[i]
     features = defaultdict(lambda : -1)
-    assert wordseq[0] == '<START>' and wordseq[-1] == '<STOP>', "<start> and <stop> tags missing"
+    assert wordseq[0][0] == '<START>' and wordseq[-1][0] == '<STOP>', "<start> and <stop> tags missing"
     if wi == '<START>' or wi == '<STOP>':
         features.update({
             wi: True
@@ -49,8 +49,6 @@ def word2features(i, wordseq):
     features.update(bow)
 
     features.update({
-        # 'Wi': wi,
-        # 'Wiaslower': wi.lower(),
         'iswialpha': wi.isalpha(),
         'iswititle': wi.istitle(),
         'iswiupper': wi.isupper(),
@@ -58,16 +56,17 @@ def word2features(i, wordseq):
         'iswidigit': wi.isdigit(),
         'iswinumeric': wi.isnumeric(),
         'Wishape': len(wi),
-        'hasspecialchar': len([w for w in wi if w in list('~!@#$%^&*()_+-={}|:"<>?,./;\'[]')]) != 0   #re.findall('[~!@#$%^&*(){}|:"<>?,./;\'\[\]]', wi) != None,
+        'hasspecialchar': len([w for w in wi if w in list('~!@#$%^&*()_+-={}|:"<>?,./;\'[]')]) != 0,
+        'isacronym': len(re.findall(r'(?:[A-Z]\.?){2,}' , wi)) > 0,
+        'postag': postag,
     })
     if i>1:
-        wiminus1 = wordseq[i-1]
+        wiminus1, posminus1 = wordseq[i-1]
         features.update({
-            # 'Wi-1': wiminus1,
             'iswiminus1title': wiminus1.istitle(),
             'iswiminus1upper': wiminus1.isupper(),
             'iswiminus1lower': wiminus1.islower(),
-            # 'Wi-1aslower': wiminus1.lower(),
+            'postagminus1': posminus1,
         })
     elif i==1:
         features.update({
@@ -75,15 +74,15 @@ def word2features(i, wordseq):
             'iswiminus1title': -1,
             'iswiminus1upper': -1,
             'iswiminus1lower': -1,
+            'postagminus1': -1,
         })
     if i<len(wordseq)-2:
-        wiplus1 = wordseq[i+1]
+        wiplus1, posplus1 = wordseq[i+1]
         features.update({
-            # 'Wi+1': wiplus1,
             'iswiplus1title': wiplus1.istitle(),
             'iswiplus1upper': wiplus1.isupper(),
             'iswiplus1lower': wiplus1.islower(),
-            # 'Wi+1aslower': wiplus1.lower(),
+            'postagplus1': posplus1,
         })
     elif i==len(wordseq)-2:
         features.update({
@@ -91,6 +90,7 @@ def word2features(i, wordseq):
             'iswiplus1title': -1,
             'iswiplus1upper': -1,
             'iswiplus1lower': -1,
+            'postagplus1': -1,
         })
 
     features.update({
@@ -98,6 +98,7 @@ def word2features(i, wordseq):
         'gaztag-LOC' : -1, 'gaztag-PER' : -1, 'gaztag-ORG' : -1, 'gaztag-MISC' : -1,
         'gaz+1tag-LOC' : -1, 'gaz+1tag-PER' : -1, 'gaz+1tag-ORG' : -1, 'gaz+1tag-MISC' : -1,
         'gaz-1tag-LOC' : -1, 'gaz-1tag-PER' : -1, 'gaz-1tag-ORG' : -1, 'gaz-1tag-MISC' : -1,  
+        'gaz3tag-LOC' : -1, 'gaz3tag-PER' : -1, 'gaz3tag-ORG' : -1, 'gaz3tag-MISC' : -1,  
     })
 
     if wi.isalnum():
@@ -106,32 +107,38 @@ def word2features(i, wordseq):
         gazminus1 = False
         gaz3 = False
         for k in g_dict.keys():
-            if wi in g_dict[k]:
+            if any(n in g_dict[k] for n in [wi.upper(), wi.lower(), wi.title(), wi.capitalize()]):
                 gaz = True
                 features.update({
                     'gaztag-'+str(k): 1,
                 })
-            if 'wiplus1' in locals() and wi+' '+wiplus1 in g_dict[k]:
-                gazplus1 = True
-                features.update({
-                    'gaz+1tag-'+str(k): 1,
-                })
-            if 'wiminus1' in locals() and wiminus1+' '+wi in g_dict[k]:
-                gazminus1 = True
-                features.update({
-                    'gaz-1tag-'+str(k): 1,
-                })
-            if 'wiminus1' in locals()  and 'wiplus1' in locals() and wiminus1+' '+wi+' '+wiplus1 in g_dict[k]:
-                gaz3 = True
-                features.update({
-                    'gaz3tag-'+str(k): 1,
-                })
-        features.update({'gaz': gaz , 'gaz+1': gazplus1 , 'gaz-1': gazminus1 , 'gaz3': gaz3})
+            if 'wiplus1' in locals():
+                wd = wi+' '+wiplus1
+                if any(name in g_dict[k] for name in [wd.upper(), wd.lower(), wd.title(), wd.capitalize()]):
+                    gazplus1 = True
+                    features.update({
+                        'gaz+1tag-'+str(k): 1,
+                    })
+            if 'wiminus1' in locals() :
+                wd = wiminus1+' '+wi
+                if any(n in g_dict[k] for n in  [wd.upper(), wd.lower(), wd.title(), wd.capitalize()]):
+                    gazminus1 = True
+                    features.update({
+                        'gaz-1tag-'+str(k): 1,
+                    })
+            if 'wiminus1' in locals() and 'wiplus1' in locals() :
+                wd = wiminus1+' '+wi+' '+wiplus1 
+                if any(n in g_dict[k] for n in  [wd.upper(), wd.lower(), wd.title(), wd.capitalize()]):
+                    gaz3 = True
+                    features.update({
+                        'gaz3tag-'+str(k): 1,
+                    })
+        features.update({'gaz': gaz*100 , 'gaz+1': gazplus1*100 , 'gaz-1': gazminus1*100 , 'gaz3': gaz3*100})
 
     return features
 
 def sent2features(sentence):
-    assert isinstance(sentence, list) and isinstance(sentence[0], str), '`sentence` should be list of words as str'
+    assert isinstance(sentence, list) and isinstance(sentence[0], tuple) and isinstance(sentence[0][0], str), '`sentence` should be list of tuple of words and tags as str'
     xs = [None]*len(sentence)
     for i in range(len(sentence)):
         xs[i] = word2features(i, sentence)
@@ -150,7 +157,7 @@ def encode_ylabel(y):
     return ty
 
 def iob_sents2Xy(iob_sents, test=False) :
-    sents_list = [['<START>']+[w for w, _, _ in wseq]+['<STOP>'] for wseq in iob_sents]
+    sents_list = [[('<START>','<START>')]+[(w,p) for w, p, _ in wseq]+[('<STOP>','<STOP>')] for wseq in iob_sents]
     s2fs = list(map(sent2features, sents_list))
     Xs = list(chain.from_iterable(s2fs))
     ys = list(chain.from_iterable([['<START>']+[e for _, _, e in wseq]+['<STOP>'] for wseq in iob_sents]))
@@ -158,30 +165,35 @@ def iob_sents2Xy(iob_sents, test=False) :
     X = feats2vects(Xs, test=test)
     return np.array(X), np.array(y), {'sents_list':sents_list, 's2fs':s2fs, 'ys':ys}
 
+def my_token_preps(user_sent):
+    pattern = r'(\s|"|:|,|:|;|\'|!|\?|\(|\)|\.$)'
+    s_l = list(filter(lambda x : ('').__ne__(x) and (' ').__ne__(x), re.split(pattern , user_sent)))
+    if s_l[-1] != '.':
+        s_l.append('.')
+    return [('<START>','<START>')]+nltk.pos_tag(s_l)+[('<STOP>','<STOP>')]
 
 if __name__ == '__main__':
     train = ConllCorpusReader('CoNLL-2003', 'eng.train', ['words', 'pos', 'ignore', 'chunk'])
     test = ConllCorpusReader('CoNLL-2003', 'eng.testa', ['words', 'pos', 'ignore', 'chunk'])
     red2v = DictVectorizer(sparse=False)
-    # varying training size
+    
     all_train = train.iob_sents()
     all_test = test.iob_sents()
     fulltrainsize = len(all_train)
     fulltestsize = len(all_test)
-    # trainsizes = [100, 500, 1000, 5000, 10000, fulltrainsize]
-    # for trainsize in trainsizes:
+    
     trainsize = 5000
     print('#'*60)
     print(f'Training size : {trainsize}')
-    ttfrac = trainsize/fulltrainsize
-    testsize = fulltestsize  #int(ttfrac*fulltestsize)
+    
+    testsize = fulltestsize
     Xtrain, ytrain, propstr = iob_sents2Xy(all_train[:trainsize])
     print('X: ',Xtrain.shape, 'y: ',ytrain.shape)
 
     # Single
-    svmclassifier = SVC(probability=True)
+    # svmclassifier = SVC(probability=True)
 
-    svmclassifier.fit(Xtrain,ytrain)
+    # svmclassifier.fit(Xtrain,ytrain)
 
     Xtest, ytest, propsts = iob_sents2Xy(all_test[:testsize], test=True)
 
@@ -189,10 +201,10 @@ if __name__ == '__main__':
 
     score = svmclassifier.score(Xtest,ytest)
 
-    with open(f'svmclassifier-{trainsize}.pkl', 'wb') as f:
-        pickle.dump(svmclassifier, f)
-    with open(f'red2v-{trainsize}.pkl', 'wb') as f:
-        pickle.dump(red2v, f)
+    # with open(f'svmclassifier-{trainsize}.pkl', 'wb') as f:
+    #     pickle.dump(svmclassifier, f)
+    # with open(f'red2v-{trainsize}.pkl', 'wb') as f:
+    #     pickle.dump(red2v, f)
 
     print(f'{(predictions == ytest).sum()} correct out of {ytest.shape[0]} entities. '
         f'Accuracy = {(predictions == ytest).sum()/ytest.shape[0]}')
@@ -201,19 +213,15 @@ if __name__ == '__main__':
     rr = 6890
     i=0
     for s in propsts['sents_list']:
-        for w in s:
+        for w, p in s:
             if i>rr:
-                print(f"{w:20s} - {propsts['ys'][i]:6s} - {predictions[i] if predictions[i]==1 else ''}")
+                print(f"{w:20s} - {p:5s} - {propsts['ys'][i]:6s} - {predictions[i] if predictions[i]==1 else ''}")
             i+=1
             if i-rr>40: break
         if i-rr>40: break
 
-    user_sent = "Ludwig van Beethoven was a G.O.A.T from Germany who said, \"Nothing is more intolerable than to have to admit to yourself your own errors!\""
-    pattern = r'(\s|"|:|,|:|;|\'|!|\?|\(|\)|\.$)'
-    s_l = list(filter(lambda x : ('').__ne__(x) and (' ').__ne__(x), re.split(pattern , user_sent)))
-    if s_l[-1] != '.':
-        s_l.append('.')
-    s_l = ['<START>'] + s_l + ['<STOP>']
+    user_sent = "Ludwig van Beethoven was a G.O.A.T from Germany who said, \"Nothing is more intolerable than to have to admit to yourself your own errors!\"\n --(1790s)"
+    s_l = my_token_preps(user_sent)
     s2f = sent2features(s_l)
     x = feats2vects(s2f, test=True)
     user_nei = svmclassifier.predict(x)
--- NER/ner_gui.py
+++ NER/ner_gui.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
-from NER import feats2vects, sent2features, svmclassifier
-import re
+from tkinter import font
+from NER import feats2vects, sent2features, svmclassifier, my_token_preps
 
 def mock_predict(x):
     # Mock prediction; replace with `svmclassifier.predict(x)` in actual use.
-    return [1 if token[0].isupper() else 0 for token in x]
+    return [1 if token.istitle()*1+(pos in ['NNP'])*2>1 else 0 for token, pos in x]
 
 # Function to perform NER on input sentence
 def predict_ner():
@@ -15,12 +15,7 @@ def predict_ner():
         return
 
     # Tokenize and prepare input
-    # s_l = user_sent.split()
-    pattern = r'(\s|"|:|,|:|;|\'|!|\?|\(|\)|\.$)'
-    s_l = list(filter(lambda x : ('').__ne__(x) and (' ').__ne__(x), re.split(pattern , user_sent)))
-    if s_l[-1] != '.':
-        s_l.append('.')
-    s_l = ['<START>'] + s_l + ['<STOP>']
+    s_l = my_token_preps(user_sent)
     
     # Feature extraction and vectorization
     s2f = sent2features(s_l)
@@ -28,12 +23,18 @@ def predict_ner():
     
     # Prediction
     user_nei =  svmclassifier.predict(x)  # mock_predict(x)  # Replace mock_predict with svmclassifier.predict
+    ume = mock_predict(s_l)
     
     # Display tokens and NER tags
     output_field.delete(*output_field.get_children())  # Clear previous entries
-    for w, e in zip(s_l, user_nei):
-        tag = "NER" if e == 1 else ""
-        output_field.insert('', 'end', values=(w, tag))
+    insert_colored_row([(w,p,e,k) for (w,p), e, k in zip(s_l, user_nei, ume)])
+
+# Function to insert color-coded rows
+def insert_colored_row(data):
+    for i, (token, pos_tag, ner_tag, k) in enumerate(data):
+        tag = "oddrow" if i % 2 == 0 else "evenrow"
+        ner_tag = "NER" if ner_tag == 1 else ""
+        output_field.insert("", "end", values=(token, pos_tag, ner_tag, k), tags=(tag,))
 
 # Function to clear input and output fields
 def clear_fields():
@@ -43,27 +44,61 @@ def clear_fields():
 # Set up the main application window
 root = tk.Tk()
 root.title("NER GUI")
-root.geometry("600x500")
+root.geometry("700x550")
 
-# Input label and field
-tk.Label(root, text="Enter Sentence:").pack(pady=5)
-input_field = tk.Text(root, height=10, width=90)
-input_field.pack()
+# Define font style and size
+font_style = font.Font(family="Helvetica", size=14)
+button_font = font.Font(family="Helvetica", size=12, weight="bold")
+treeview_font = font.Font(family="Helvetica", size=12)
 
-# Buttons
+# Input label and field with updated font
+tk.Label(root, text="Enter Sentence:", font=font_style).pack(pady=5)
+input_field = tk.Text(root, height=5, width=60, font=font_style)
+input_field.pack(pady=(0, 15))
+
+# Buttons with updated font
 button_frame = tk.Frame(root)
 button_frame.pack(pady=10)
-predict_button = tk.Button(button_frame, text="Predict NER", command=predict_ner)
+predict_button = tk.Button(button_frame, text="Predict NER", command=predict_ner, font=button_font)
 predict_button.grid(row=0, column=0, padx=10)
-clear_button = tk.Button(button_frame, text="Clear", command=clear_fields)
+clear_button = tk.Button(button_frame, text="Clear", command=clear_fields, font=button_font)
 clear_button.grid(row=0, column=1, padx=10)
 
-# Output label and treeview for displaying tokens and tags
-tk.Label(root, text="NER Output:").pack(pady=5)
-output_field = ttk.Treeview(root, columns=("Token", "NER Tag"), show="headings", height=10)
-output_field.heading("Token", text="Token")
-output_field.heading("NER Tag", text="NER Tag")
-output_field.pack()
+# Output label
+tk.Label(root, text="NER Output:", font=font_style).pack(pady=5)
+
+# Frame for output field and scrollbar
+output_frame = tk.Frame(root)
+output_frame.pack(fill="both", expand=True)
+
+# Scrollbar
+scrollbar = tk.Scrollbar(output_frame)
+scrollbar.pack(side="right", fill="y")
+
+# Treeview for displaying tokens and tags with larger font
+output_field = ttk.Treeview(output_frame, columns=("Token", "POS Tag", "NER Tag", 'ku'), show="headings", height=10, yscrollcommand=scrollbar.set)
+output_field.heading("Token", text="Token", anchor="w")
+output_field.heading("POS Tag", text="POS Tag", anchor="w")
+output_field.heading("NER Tag", text="NER Tag", anchor="w")
+output_field.heading("ku", text="", anchor="w")
+output_field.column("Token", anchor="w", width=180)
+output_field.column("POS Tag", anchor="w", width=120)
+output_field.column("NER Tag", anchor="w", width=120)
+output_field.column("ku", anchor="w", width=12)
+output_field.pack(fill="both", expand=True)
+
+# Configure the scrollbar
+scrollbar.config(command=output_field.yview)
+
+# Apply the font to the Treeview rows
+style = ttk.Style()
+style.configure("Treeview", font=treeview_font, rowheight=30)
+style.configure("Treeview.Heading", font=button_font)
+
+# Alternate row colors
+style.map("Treeview", background=[("selected", "#DDEEFF")])
+output_field.tag_configure("oddrow", background="#f0f0f0")
+output_field.tag_configure("evenrow", background="#ffffff")
 
 # Run the main loop
 root.mainloop()
